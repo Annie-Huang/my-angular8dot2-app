6eb39145482bbff2f6bd619f5dfc957adcad0e3e
Annie Huang <annie.hn@gmail.com>
2020-05-31 22:26:42 +1000

    Test using a lib component inside the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { EaUiModule } from '@ea/ea-ui';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EaUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
